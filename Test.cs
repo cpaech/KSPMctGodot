18d2b3512df85fae5dc89d83c02159ab443611d0
Constantin Paech <constantin.a.paech@gmail.com>
2024-02-02 17:19:02 +0100

    KRPC-Test

--- Test.cs
+++ Test.cs
@@ -1,12 +1,24 @@
 using Godot;
+using System.Net;
+using KRPC.Client;
+using KRPC.Client.Services.KRPC;
+
 using System;
 
 public partial class Test : Node
 {
+	[Export]
+    public string ip = "127.0.0.1";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("All is working");
+        using (var connection = new Connection(
+                   name: "My Example Program",
+                   address: IPAddress.Parse(ip),
+                   rpcPort: 5000,
+                   streamPort: 5001)) {
+            var krpc = connection.KRPC();
+            GD.Print(krpc.GetStatus().Version);}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
