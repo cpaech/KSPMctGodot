e731dfe067aab98bb4f3ff60671b2db871c1a6ce
Constantin Paech <constantin.a.paech@gmail.com>
2024-02-02 17:59:38 +0100

    Redo

---
+++ components/navball/navball_ball.cs
@@ -0,0 +1,17 @@
+using Godot;
+using KRPC.Client;
+using KRPC.Client.Services.SpaceCenter;
+using System;
+using System.Net;
+public partial class navball_ball : CsgSphere3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
