fe6d9b0d51dfac44db14b5e873137f374928cdd6
moriati <90148341+M0riati@users.noreply.github.com>
2024-02-02 17:45:30 +0100

    Singleton

--- Test.cs
+++ Test.cs
@@ -8,17 +8,22 @@ using System;
 public partial class Test : Node
 {
 	[Export]
-    public string ip = "127.0.0.1";
+	public string ip = "127.0.0.1";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        using (var connection = new Connection(
-                   name: "My Example Program",
-                   address: IPAddress.Parse(ip),
-                   rpcPort: 5000,
-                   streamPort: 5001)) {
-            var krpc = connection.KRPC();
-            GD.Print(krpc.GetStatus().Version);}
+		GD.Print("Test");
+		using (var connection = new Connection(
+				   name: "My Example Program",
+				   address: IPAddress.Parse(ip),
+				   rpcPort: 5000,
+				   streamPort: 5001)) {
+			var krpc = connection.KRPC();
+			GD.Print(krpc.GetStatus().Version);}
+	}
+
+	public void GetTest() {
+		GD.Print("It WORKS!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
