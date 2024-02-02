2535329e3ea8493764ae8f7595243b4cfbdb9c76
Constantin Paech <constantin.a.paech@gmail.com>
2024-02-02 18:18:06 +0100

    Working Dev Build

---
+++ KRPCManager.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System.Net;
+using KRPC.Client;
+using KRPC.Client.Services.KRPC;
+using KRPC.Client.Services.SpaceCenter;
+
+using System;
+
+public partial class KRPCManager : Godot.Node
+{
+	[Export]
+	public string ip = "127.0.0.1";
+	public Connection krpcConnection;
+	public KRPC.Client.Services.SpaceCenter.Service spaceCenter;
+	public KRPC.Client.Services.SpaceCenter.Vessel currentVessel;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		krpcConnection = new Connection(
+				   name: "My Example Program",
+				   address: IPAddress.Parse(ip),
+				   rpcPort: 5000,
+				   streamPort: 5001);
+		spaceCenter = krpcConnection.SpaceCenter();
+		currentVessel = spaceCenter.ActiveVessel;
+	}
+
+}
--- components/navball/navball_ball.cs
+++ components/navball/navball_ball.cs
@@ -5,9 +5,12 @@ using System;
 using System.Net;
 public partial class navball_ball : CsgSphere3D
 {
+	private KRPCManager kRPC;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+    	kRPC = (KRPCManager) GetNode("/root/KrpcManager");
+		GD.Print(kRPC.currentVessel.Name);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
